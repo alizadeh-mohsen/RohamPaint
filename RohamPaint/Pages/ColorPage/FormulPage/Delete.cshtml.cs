40bdeaa151a54fbeced53ebfaa5d64c69e79085d
Mohsen Alizadeh <kelarens@gmail.com>
2026-02-21 09:54:58 +0000

    added styling to formul page

--- RohamPaint/Pages/ColorPage/FormulPage/Delete.cshtml.cs
+++ RohamPaint/Pages/ColorPage/FormulPage/Delete.cshtml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using RohamPaint.Data;
 using RohamPaint.Models;
 
 namespace RohamPaint.Pages.ColorPage.FormulPage
@@ -57,7 +52,7 @@ namespace RohamPaint.Pages.ColorPage.FormulPage
                 await _context.SaveChangesAsync();
             }
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("../Index");
         }
     }
 }
